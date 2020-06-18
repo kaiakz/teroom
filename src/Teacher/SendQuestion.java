0a649e26844bd79292c4ff8c37b49e17974d151c
XuZhongChun <2359579934@qq.com>
2020-06-18 21:29:52 +0800

    Update question modules

--- src/Teacher/SendQuestion.java
+++ src/Teacher/SendQuestion.java
@@ -9,12 +9,12 @@ package Teacher;
  *
  * @author 123
  */
-public class Question extends javax.swing.JFrame {
+public class SendQuestion extends javax.swing.JFrame {
 
     /**
      * Creates new form Question
      */
-    public Question() {
+    public SendQuestion() {
         initComponents();
         setTitle("答题卡");
         setLocationRelativeTo(null);
@@ -100,20 +100,21 @@ public class Question extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Question.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SendQuestion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Question.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SendQuestion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Question.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SendQuestion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Question.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SendQuestion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Question().setVisible(true);
+                new SendQuestion().setVisible(true);
             }
         });
     }
